0123e007abfe170c41a570b7a3e042ff60eff4b5
Jake911 <infernioboss@yandex.ru>
2018-10-30 17:02:23 +0300

    lul))

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React YEAAAH
+            Learn React YEAAAH nooooooo
           </a>
         </header>
       </div>
