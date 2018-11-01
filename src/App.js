421bb8cd0ab02c4fbdf1b3d0ffce2975c2e76b45
Andrew <andsmi97@yandex.ru>
2018-11-01 21:10:03 +0300

    accordeon default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
+import Accordion from './Components/AccordionSection/Accordion'
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div>
+          <h1>Accordion Demo</h1>
+          <Accordion>
+            <div label="Alligator Mississippiensis">
+              <p>
+                <strong>Common Name:</strong> American Alligator
+              </p>
+              <p>
+                <strong>Distribution:</strong> Texas to North Carolina, US
+              </p>
+              <p>
+                <strong>Endangered Status:</strong> Currently Not Endangered
+              </p>
+            </div>
+            <div label="Alligator Sinensis">
+              <p>
+                <strong>Common Name:</strong> Chinese Alligator
+              </p>
+              <p>
+                <strong>Distribution:</strong> Eastern China
+              </p>
+              <p>
+                <strong>Endangered Status:</strong> Critically Endangered
+              </p>
+            </div>
+          </Accordion>
+        </div>
       </div>
     );
   }
